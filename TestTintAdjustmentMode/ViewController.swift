2d731f3f710c6fa953a28619c2465ab859f85435
Bart van Kuik <bart@dutchvirtual.nl>
2017-03-03 09:50:02 +0100

    First version: button with image is not tint-adjusted when popover appears

---
+++ TestTintAdjustmentMode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTintAdjustmentMode
+//
+//  Created by Bart van Kuik on 03/03/2017.
+//  Copyright © 2017 DutchVirtual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
